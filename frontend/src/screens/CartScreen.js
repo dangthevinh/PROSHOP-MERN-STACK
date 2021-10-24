65ea0f4e598aa1c42c6133c3d9617bef4887c3ed
The Vinh Dang <kaiovinh@gmail.com>
2021-10-24 17:54:18 +0700

    upload source code

---
+++ frontend/src/screens/CartScreen.js
@@ -0,0 +1,12 @@
+//jshint esversion:10
+import React from 'react';
+
+const CartScreen = () => {
+    return (
+        <div>
+            Cart
+        </div>
+    )
+}
+
+export default CartScreen;
